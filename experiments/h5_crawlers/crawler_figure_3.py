8363ced3e3d18d49090d91d938d013eacc41d636
bmiselis <bartosz.miselis@gmail.com>
2019-12-10 19:37:39 -0500

    - crawlers for h5 results from the experiments - csv files used to produce figures in our reproducibility report

---
+++ experiments/h5_crawlers/crawler_figure_3.py
@@ -0,0 +1,70 @@
+import os
+
+import h5py
+import numpy as np
+import pandas as pd
+
+source_dir = 'results/iter_lot_fc_orig'
+
+seeds = [0, 1, 2, 3, 4]
+
+index = 0
+df = pd.DataFrame(
+    columns=['mask_criterion', 'seed', 'iteration', 'test_accuracy'],
+    index=np.arange(1125)
+)
+
+criteria_to_extract = [
+    'magnitude_increase',
+    'movement',
+    'large_final',
+    'small_final',
+    'large_init',
+    'small_init',
+    'large_init_large_final',
+    'small_init_small_final',
+    'random',
+]
+
+for seed in seeds:
+    seed_dir = os.path.join(source_dir, 'test_seed_{}'.format(seed))
+    mask_criteria = [el for el in os.listdir(seed_dir)
+                     if os.path.isdir(os.path.join(seed_dir, el))]
+
+    original_weights_file = os.path.join(seed_dir, 'weights')
+
+    with h5py.File(original_weights_file, 'r') as data:
+        idx_min_val_loss = np.argmin(data['val_loss'][...])
+        original_test_accuracy = data['test_accuracy'][...][idx_min_val_loss]
+
+    for mask_criterion in mask_criteria:
+        if mask_criterion[:-27] not in criteria_to_extract:
+            continue
+
+        mask_dir = os.path.join(seed_dir, mask_criterion)
+        iterations = os.listdir(mask_dir)
+        iterations = list(map(lambda x: x[2:], iterations))
+        iterations.insert(0, 0)  # Placeholder for unpruned network accuracy.
+        iterations = sorted(iterations, key=int)
+
+        for n, iteration in enumerate(iterations, start=0):
+            if n == 0:
+                df.iloc[index] = [
+                    mask_criterion[:-27],
+                    seed,
+                    0,
+                    original_test_accuracy[0],
+                ]
+                index += 1
+            else:
+                iteration_dir = os.path.join(mask_dir, 'pp' + iteration)
+                weights_file = os.path.join(iteration_dir, 'weights')
+                with h5py.File(weights_file, 'r') as data:
+                    idx_min_val_loss = np.argmin(data['val_loss'][...])
+                    test_accuracy = data['test_accuracy'][...][idx_min_val_loss]
+                    df.iloc[index] = [
+                        mask_criterion[:-27], seed, n, test_accuracy[0],
+                    ]
+                    index += 1
+
+df.to_csv('results/crawled_data_figure_3.csv')
---
+++ experiments/h5_crawlers/crawler_figure_fc_early_stopped_0.75_val_acc.py
@@ -0,0 +1,56 @@
+import os
+
+import h5py
+import numpy as np
+import pandas as pd
+
+source_dir = 'results/early_stopped_fc_0.75'
+
+seeds = [0, 1, 2, 3, 4]
+final_weights_inds = [13, 15, 13, 15, 11]
+
+index = 0
+df = pd.DataFrame(
+    columns=['mask_criterion', 'seed', 'iteration', 'test_accuracy'],
+    index=np.arange(1625)
+)
+
+mask_criteria = [
+    'large_final',
+    'small_final',
+    'random',
+    'large_init',
+    'small_init',
+    'large_init_large_final',
+    'small_init_small_final',
+    'magnitude_increase',
+    'movement',
+    'large_final_diff_sign',
+    'large_final_same_sign',
+    'magnitude_increase_diff_sign',
+    'magnitude_increase_same_sign',
+]
+
+for seed, final_weights_ind in zip(seeds, final_weights_inds):
+    seed_dir = os.path.join(source_dir, 'test_seed_{}'.format(seed))
+
+    results_file = os.path.join(seed_dir,
+                                'mask_scenarios',
+                                'all_init_accuracy_lot_exp_none_fw_ind_{}'.format(final_weights_ind),
+                                )
+
+    with h5py.File(results_file, 'r') as results:
+        test_accuracies = results['test_accuracy'][...]
+        for n, mask_criterion in enumerate(mask_criteria):
+            for percentile in range(25):  # 0 as a percentile, too.
+                percentile_test_acc = test_accuracies[percentile * 13 + n][0]
+
+                df.iloc[index] = [
+                    mask_criterion,
+                    seed,
+                    percentile,
+                    percentile_test_acc,
+                ]
+                index += 1
+
+df.to_csv('results/crawled_data_early_stopped_0.75_val_acc.csv')
